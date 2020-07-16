6abb4b636a69e6268dfa978108ff3316a062a9a9
danielt998 <daniel@fearnley.net>
2020-07-16 22:23:56 +0100

    add flashcard type selector

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 //import BpkInput from 'bpk-component-input';
 import BpkTextarea from 'bpk-component-textarea';
+import BpkButton from 'bpk-component-button';
+import BpkRadio from 'bpk-component-radio';
+//import OutputSelector from './OutputSelector'
+import BpkSelect from 'bpk-component-select';
+import BpkLabel from 'bpk-component-label';
 
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -11,6 +15,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <BpkTextarea rows="10" />
+        <BpkLabel>Flashcard type</BpkLabel>
+        <BpkSelect>
+          <option value="pleco">Pleco</option>
+          <option value="anki">Anki</option>
+        </BpkSelect>
+        {/* <OutputSelector/> */}
+        <BpkButton>Generate Flashcards</BpkButton>
       </header>
 
     </div>
