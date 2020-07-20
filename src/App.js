e01184653cdcd7de281af8101d33afee73afd501
danielt998 <daniel@fearnley.net>
2020-07-20 19:58:57 +0100

    turn App into component and wire up flashcard type options

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
-//import BpkInput from 'bpk-component-input';
 import BpkTextarea from 'bpk-component-textarea';
 import BpkButton from 'bpk-component-button';
-import BpkRadio from 'bpk-component-radio';
-//import OutputSelector from './OutputSelector'
 import BpkSelect from 'bpk-component-select';
 import BpkLabel from 'bpk-component-label';
 
 import './App.css';
 
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {flashcardType:"pleco"}
+  }
+  render () {
+    return (
     <div className="App">
       <header className="App-header">
         <BpkTextarea rows="10" />
         <BpkLabel>Flashcard type</BpkLabel>
-        <BpkSelect>
+        <BpkSelect onChange={(e) => this.state=e.target.value}>
           <option value="pleco">Pleco</option>
           <option value="anki">Anki</option>
         </BpkSelect>
-        {/* <OutputSelector/> */}
         <BpkButton>Generate Flashcards</BpkButton>
+
       </header>
 
     </div>
-  );
+    );
+  }
 }
 
 export default App;
